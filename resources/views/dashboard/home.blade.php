45bea63bc5ff45dfc23d87c19154a83cfe165caf
Jlafratta <julian.lafratta@gmail.com>
2020-08-30 00:04:02 -0300

    Template applied

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.dash')
+
+@section('content')
+
+<div class="app-main__outer">
+    <div class="app-main__inner">
+
+        {{-- TITLE SECTION --}}
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <div class="font-icon-lg">
+                            <i class="fa fa-cart-plus " aria-hidden="true"></i>
+                        </div>
+                    </div>
+                    <div>Nuevo ingreso
+                        <div class="page-title-subheading">Dar de alta una nueva venta.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  {{-- end title --}}   
+
+        {{-- CONTENT --}}
+        <div class="tab-content">
+            <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
+                <div class="row">
+
+                    <div class="col-md-6">
+                        <div class="main-card mb-3 card">
+                            <div class="card-body"><h5 class="card-title">Cliente</h5><br>
+                                <div class="row">
+
+                                    <div class="col-md-6">
+                                        <div class="position-relative form-group">
+                                            <label for="exampleCustomSelect" class="">Nombre</label>
+                                            <select type="select" id="exampleCustomSelect" name="customSelect" class="custom-select">
+                                                <option value="">Seleccionar</option>
+                                                <option>Value 1</option>
+                                                <option>Value 2</option>
+                                                <option>Value 3</option>
+                                                <option>Value 4</option>
+                                                <option>Value 5</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="position-relative form-group">
+                                            <label for="exampleCustomSelect" class="">Vehiculo</label>
+                                            <select type="select" id="exampleCustomSelect" name="customSelect" class="custom-select">
+                                                <option value="">Seleccionar</option>
+                                                <option>Value 1</option>
+                                                <option>Value 2</option>
+                                                <option>Value 3</option>
+                                                <option>Value 4</option>
+                                                <option>Value 5</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="main-card mb-3 card">
+                            <div class="card-body"><h5 class="card-title">Producto</h5><br>
+                                <form class="">
+                                    <fieldset class="position-relative form-group">
+                                        <div class="position-relative form-check">
+                                            <label class="form-check-label">
+                                                <input name="radio1" type="radio" class="form-check-input" checked="checked"> Hielo
+                                            </label>
+                                        </div>
+                                    </fieldset>
+                                    <div class="text-right">
+                                        <button class="mt-2 btn btn-primary btn-lg">Finalizar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="main-card mb-3 card">
+                            <div class="card-body"><h5 class="card-title">Balanza</h5><br>
+                                <form class="">
+                                    <div class="input-group mb-2 form-control-lg">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text ">Bruto</span>
+                                        </div>
+                                        <input value="0.0" type="text" class="form-control form-control-lg">
+                                    </div>
+                                    <br>
+                                    <div class="input-group mb-2 form-control-lg">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Tara&nbsp;&nbsp;</span>
+                                        </div>
+                                        <input value="0.0" type="text" class="form-control form-control-lg">
+                                    </div>
+                                    <br>
+                                    <div class="input-group mb-2 form-control-lg">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Neto</span>
+                                        </div>
+                                        <input value="0.0" type="text" class="form-control form-control-lg">
+                                    </div>
+                                    
+                                    <div class="mt-4 divider"></div>
+                                    <div class="mt-4 text-right"><h5>$ 1200</h6></div>
+                                    
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/dash.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Analytics Dashboard - This is an example dashboard created using build-in elements and components.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+    <meta name="msapplication-tap-highlight" content="no">
+    <!--
+    =========================================================
+    * ArchitectUI HTML Theme Dashboard - v1.0.0
+    =========================================================
+    * Product Page: https://dashboardpack.com
+    * Copyright 2019 DashboardPack (https://dashboardpack.com)
+    * Licensed under MIT (https://github.com/DashboardPack/architectui-html-theme-free/blob/master/LICENSE)
+    =========================================================
+    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    -->
+
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet"> <!--load all icons styles -->
+    
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    @yield('css')
+</head>
+<body>
+    {{-- APP --}}
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+
+        {{-- HEADER --}}
+        @include('layouts.header')
+
+        {{-- THEME SETTINGS (disabled) --}}  
+
+        {{-- MAIN --}}
+        <div class="app-main">
+            
+            {{-- SIDEBAR --}}
+            @include('layouts.sidebar')
+
+            {{-- CONTENT --}}
+            @yield('content')
+            
+            <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div> {{-- end main --}}
+    </div> {{-- end app --}}
+
+    {{-- JS --}}
+    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+    @yield('javascript')
+
+</body>
+</html>
