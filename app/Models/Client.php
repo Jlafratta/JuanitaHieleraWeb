b7a8fcd75c79246adbf167bbf233975a53c9e9f0
Jlafratta <julian.lafratta@gmail.com>
2020-09-03 04:59:05 -0300

    tables mapped

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    public function vehicles(){
+        return $this->hasMany('App\Models\Vehicle');
+    }
 }
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function client(){
+        return $this->hasOne('App\Models\Client');
+    }
+
+    public function vehicle(){
+        return $this->hasOne('App\Models\Vehicle');
+    }
 }
